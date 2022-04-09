ac7154db46554c6cfa4da57bf90d3f5fe0ad8102
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-04-09 18:31:30 +0400

    Init

---
+++ Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Library.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PageCount { get; set; }
+        public String PublisherName { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime WrittenDate { get; set; }
+        public Author Author { get; set; }
+
+        public void Update(Book book)
+        {
+            this.Name = book.Name ?? this.Name;
+            this.PageCount = book.PageCount == 0 ? this.PageCount : book.PageCount;
+         // TODO: add fields   
+        }
+
+        public override String ToString()
+        {
+            return Id + " " + Name + " " + PageCount;
+        }
+    }
+}
